fc51a073e81f769bba055ae41464bfc83756a76e
Hackbright Student <no-reply@hackbrightacademy.com>
2018-10-26 16:52:40 -0700

    finished weather forcast

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -19,15 +19,42 @@ $('#get-fortune-button').on('click', showFortune);
 // PART 2: SHOW WEATHER
 
 function showWeather(evt) {
+
+	// write this line to load run stuff on an order rather than on the way js optimize 
     evt.preventDefault();
 
+    // make a variable to put the /url that we want to run in ajax 
     let url = "/weather.json";
+
+    // get user input form data  
     let formData = {"zipcode": $("#zipcode-field").val()};
 
+    	// send form data to server 
+    	// first orange result all the way to } is the call back function 
+    	// first orange result = parameter to our inline function 
+    	// result is the object AKA dictionary that the weather.json returns 
+    	// json returns a dictunary 
+    	// send data from user to the route in server to get the result 
+    	$.get(url,formData, (results) => {
+    	const weather = results['temp'] + ":" + results['forecast'];
 
+    	//Display result from server 
+    	$('#weather-info').html(weather);
+
+    	// add print for debugging  
+    	console.dir(results);
+    
+    	// display an alart
+		 alert(`The forcast for zipcode ${formData.zipcode} is ${results.forecast}`);
+		});
     // TODO: request weather with that URL and show the forecast in #weather-info
 }
 
+function displayResults(results) {
+
+}
+
+// call back on submit has to be outside of function 
 $("#weather-form").on('submit', showWeather);
 
 
