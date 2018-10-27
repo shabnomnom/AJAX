16c34acfbfd867f1e4b40688d1dd906075f5df73
Hackbright Student <no-reply@hackbrightacademy.com>
2018-10-26 18:18:40 -0700

    weather forcast completed

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -26,7 +26,8 @@ function showWeather(evt) {
     // make a variable to put the /url that we want to run in ajax 
     let url = "/weather.json";
 
-    // get user input form data  
+    // get user input form data using .val method of jquery to extract what 
+    // is in the form before before having it passed to the server  
     let formData = {"zipcode": $("#zipcode-field").val()};
 
     	// send form data to server 
@@ -50,9 +51,34 @@ function showWeather(evt) {
     // TODO: request weather with that URL and show the forecast in #weather-info
 }
 
-function displayResults(results) {
+// function showWeather(evt) {
 
-}
+// 	// write this line to load run stuff on an order rather than on the way js optimize 
+//     evt.preventDefault();
+
+//     // make a variable to put the /url that we want to run in ajax 
+//     let url = "/weather.json";
+
+//     // get user input form data  
+//     let formData = {"zipcode": $("#zipcode-field").val()};
+
+//     	$.get(url,formData, displayResults);
+//     // TODO: request weather with that URL and show the forecast in #weather-info
+// }
+
+
+// function displayResults(results) {
+// 	const weather = results['temp'] + ":" + results['forecast'];
+
+//     	//Display result from server 
+//     	$('#weather-info').html(weather);
+
+//     	// add print for debugging  
+//     	console.dir(results);
+    
+//     	// display an alart
+// 		 alert(`The forcast is ${results.forecast}`);
+// }
 
 // call back on submit has to be outside of function 
 $("#weather-form").on('submit', showWeather);
